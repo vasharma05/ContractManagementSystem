00b071488304ab9c88de34f8864d930ecf416631
vineets740 <vineets740@gmail.com>
2019-06-10 00:37:33 +0530

    Sign Up page was made along with the connection to the server

---
+++ register.php
@@ -0,0 +1,28 @@
+<?php   
+
+$con = mysqli_connect("localhost","root", "", "cms");
+if(!$con){
+    echo "Failed".mysqli_connect_errno();
+}
+
+$username =  mysqli_real_escape_string($con, $_GET["username"]);
+$name =  mysqli_real_escape_string($con, $_GET["name"]);
+$gender =  mysqli_real_escape_string($con, $_GET["gender"]);
+$designation = mysqli_real_escape_string($con, $_GET["designation"]);
+$email = mysqli_real_escape_string($con, $_GET["email"]);
+$contact = mysqli_real_escape_string($con, $_GET["contact"]);
+$password = mysqli_real_escape_string($con, $_GET["password"]);
+
+
+$query = "INSERT INTO employees VALUES ('$username','$name','$gender', '$designation', '$email', '$contact', '$password')";
+
+$result = mysqli_query($con,$query);
+
+if($result===false){
+    echo "error";
+    echo mysqli_error($con);
+}else{
+    echo "Updated";
+}
+
+?>
